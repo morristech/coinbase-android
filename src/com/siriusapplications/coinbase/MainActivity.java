17a6b3afe0fd5dd89c5554d0757d2a650efd2f6e
Isaac Waller <isaac@siriusapplications.com>
2013-02-19 18:04:14 -0800

    Added support for barcode scanning and bitcoin: URIS

--- src/com/siriusapplications/coinbase/MainActivity.java
+++ src/com/siriusapplications/coinbase/MainActivity.java
@@ -2,8 +2,10 @@ package com.siriusapplications.coinbase;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
@@ -11,6 +13,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
@@ -20,8 +23,8 @@ import com.siriusapplications.coinbase.api.LoginManager;
 
 @RequiresAuthentication
 public class MainActivity extends CoinbaseActivity implements ActionBar.TabListener {
-  
-  public class SignOutFragment extends DialogFragment {
+
+  public static class SignOutFragment extends DialogFragment {
 
 
     @Override
@@ -33,7 +36,7 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
       builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
         public void onClick(DialogInterface dialog, int id) {
           // Sign out
-          changeAccount(-1);
+          ((MainActivity) getActivity()).changeAccount(-1);
         }
       });
 
@@ -50,12 +53,19 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
 
   SectionsPagerAdapter mSectionsPagerAdapter;
   ViewPager mViewPager;
+  TransactionsFragment mTransactionsFragment;
+  BuySellFragment mBuySellFragment;
+  TransferFragment mTransferFragment;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    
+
+    mTransactionsFragment = new TransactionsFragment();
+    mBuySellFragment = new BuySellFragment();
+    mTransferFragment = new TransferFragment();
+
     // Create the adapter that will return a fragment for each of the three primary sections
     // of the app.
     mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -88,6 +98,21 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
           .setText(mSectionsPagerAdapter.getPageTitle(i))
           .setTabListener(this));
     }
+
+    onNewIntent(getIntent());
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+
+    super.onNewIntent(intent);
+    setIntent(intent);
+
+    if(intent.getData() != null && "bitcoin".equals(intent.getData().getScheme())) {
+      // Handle bitcoin: URI
+      mViewPager.setCurrentItem(2); // Switch to transfer fragment
+      mTransferFragment.fillFormForBitcoinUri(getIntent().getData());
+    }
   }
 
   @Override
@@ -99,7 +124,7 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    
+
     switch(item.getItemId()) {
     case R.id.menu_accounts:
       new AccountsFragment().show(getSupportFragmentManager(), "accounts");
@@ -110,8 +135,11 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
     case R.id.menu_about:
       startActivity(new Intent(this, AboutActivity.class));
       return true;
+    case R.id.menu_barcode:
+      startBarcodeScan();
+      return true;
     }
-    
+
     return super.onOptionsItemSelected(item);
   }
 
@@ -142,9 +170,9 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
     @Override
     public Fragment getItem(int i) {
       switch (i) {
-      case 0: return new TransactionsFragment();
-      case 1: return new BuySellFragment();
-      case 2: return new TransferFragment();
+      case 0: return mTransactionsFragment;
+      case 1: return mBuySellFragment;
+      case 2: return mTransferFragment;
       }
       return null;
     }
@@ -164,15 +192,54 @@ public class MainActivity extends CoinbaseActivity implements ActionBar.TabListe
       return null;
     }
   }
-  
+
   public void changeAccount(int account) {
-    
+
     if(account == -1) {
-      
+
       // Delete current account
       LoginManager.getInstance().deleteCurrentAccount(this);
       finish();
       startActivity(new Intent(this, LoginActivity.class));
+    }
+  }
+
+  public void startBarcodeScan() {
+
+    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+    intent.setPackage("com.google.zxing.client.android");
+    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+
+    try {
+      startActivityForResult(intent, 0);
+    } catch(ActivityNotFoundException e) {
+      // Barcode Scanner is not installed.
+      intent = new Intent(Intent.ACTION_VIEW);
+      intent.setData(Uri.parse("http://play.google.com/store/apps/details?id=com.google.zxing.client.android"));
+      startActivity(intent);
+    }
+  }
+
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    if (requestCode == 0) {
+      /*
+       * Barcode scan
+       */
+      if (resultCode == RESULT_OK) {
+
+        String contents = intent.getStringExtra("SCAN_RESULT");
+        String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+        Uri uri = Uri.parse(contents);
+        if(uri != null && "bitcoin".equals(uri.getScheme())) {
+          // Is bitcoin URI
+          mViewPager.setCurrentItem(2); // Switch to transfer fragment
+          mTransferFragment.fillFormForBitcoinUri(uri);
+        }
+
+      } else if (resultCode == RESULT_CANCELED) {
+        // Barcode scan was cancelled
+      }
     }
   }
 }
--- src/com/siriusapplications/coinbase/TransferFragment.java
+++ src/com/siriusapplications/coinbase/TransferFragment.java
@@ -1,36 +1,50 @@
 package com.siriusapplications.coinbase;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Arrays;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class TransferFragment extends Fragment {
-  
+
   private enum TransferType {
     SEND(R.string.transfer_send_money),
     REQUEST(R.string.transfer_request_money);
-    
+
     private int mFriendlyName;
-    
+
     private TransferType(int friendlyName) {
-      
+
       mFriendlyName = friendlyName;
     }
-    
+
     public int getName() {
-      
+
       return mFriendlyName;
     }
   }
-  
-  private Spinner mTransferType;
+
+  private Spinner mTransferTypeView;
+  private Button mSubmitSend, mSubmitEmail, mSubmitQr, mSubmitNfc;
+  private EditText mAmountView, mNotesView, mRecipientView;
+
+  private int mTransferType;
+  private String mAmount, mNotes, mRecipient;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -41,38 +55,182 @@ public class TransferFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    
+
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_transfer, container, false);
-    
-    mTransferType = (Spinner) view.findViewById(R.id.transfer_money_type);
+
+    mTransferTypeView = (Spinner) view.findViewById(R.id.transfer_money_type);
+    mTransferTypeView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+      @Override
+      public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+          long arg3) {
+
+        onTypeChanged();
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> arg0) {
+        // Will never happen.
+        throw new RuntimeException("onNothingSelected triggered on transfer type spinner");
+      }
+    });
     initializeTypeSpinner();
-    
+
+    mSubmitSend = (Button) view.findViewById(R.id.transfer_money_button_send);
+    mSubmitEmail = (Button) view.findViewById(R.id.transfer_money_button_email);
+    mSubmitQr = (Button) view.findViewById(R.id.transfer_money_button_qrcode);
+    mSubmitNfc = (Button) view.findViewById(R.id.transfer_money_button_nfc);
+
+    mAmountView = (EditText) view.findViewById(R.id.transfer_money_amt);
+    mNotesView = (EditText) view.findViewById(R.id.transfer_money_notes);
+    mRecipientView = (EditText) view.findViewById(R.id.transfer_money_recipient);
+
+    mAmountView.addTextChangedListener(new TextWatcher() {
+
+      @Override
+      public void afterTextChanged(Editable s) {
+      }
+
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count,
+          int after) {
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+        mAmount = s.toString();
+      }
+    });
+
+    mNotesView.addTextChangedListener(new TextWatcher() {
+
+      @Override
+      public void afterTextChanged(Editable s) {
+      }
+
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count,
+          int after) {
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+        mNotes = s.toString();
+      }
+    });
+
+    mRecipientView.addTextChangedListener(new TextWatcher() {
+
+      @Override
+      public void afterTextChanged(Editable s) {
+      }
+
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count,
+          int after) {
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+        mRecipient = s.toString();
+      }
+    });
+
+    mTransferTypeView.setSelection(mTransferType);
+    mAmountView.setText(mAmount);
+    mNotesView.setText(mNotes);
+    mRecipientView.setText(mRecipient);
+
     return view;
   }
 
   private void initializeTypeSpinner() {
-    
+
     ArrayAdapter<TransferType> arrayAdapter = new ArrayAdapter<TransferType>(
         getActivity(), R.layout.fragment_transfer_type, Arrays.asList(TransferType.values())) {
 
-          @Override
-          public View getView(int position, View convertView, ViewGroup parent) {
-            
-            TextView view = (TextView) super.getView(position, convertView, parent);
-            view.setText(getActivity().getString(TransferType.values()[position].getName()));
-            return view;
-          }
+      @Override
+      public View getView(int position, View convertView, ViewGroup parent) {
 
-          @Override
-          public View getDropDownView(int position, View convertView, ViewGroup parent) {
-            
-            TextView view = (TextView) super.getDropDownView(position, convertView, parent);
-            view.setText(getActivity().getString(TransferType.values()[position].getName()));
-            return view;
-          }
+        TextView view = (TextView) super.getView(position, convertView, parent);
+        view.setText(getActivity().getString(TransferType.values()[position].getName()));
+        return view;
+      }
+
+      @Override
+      public View getDropDownView(int position, View convertView, ViewGroup parent) {
+
+        TextView view = (TextView) super.getDropDownView(position, convertView, parent);
+        view.setText(getActivity().getString(TransferType.values()[position].getName()));
+        return view;
+      }
     };
     arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    mTransferType.setAdapter(arrayAdapter);
+    mTransferTypeView.setAdapter(arrayAdapter);
+  }
+
+  private void onTypeChanged() {
+
+    TransferType type = (TransferType) mTransferTypeView.getSelectedItem();
+    mTransferType = mTransferTypeView.getSelectedItemPosition();
+    boolean isSend = type == TransferType.SEND;
+
+    mSubmitSend.setVisibility(isSend ? View.VISIBLE : View.GONE);
+    mSubmitEmail.setVisibility(isSend ? View.GONE : View.VISIBLE);
+    mSubmitQr.setVisibility(isSend ? View.GONE : View.VISIBLE);
+    mSubmitNfc.setVisibility(isSend ? View.GONE : View.VISIBLE);
+    mRecipientView.setVisibility(isSend ? View.VISIBLE : View.GONE);
+  }
+
+  public void fillFormForBitcoinUri(Uri uri) {
+
+    String address = uri.getSchemeSpecificPart().split("\\?")[0];
+    String amount = null, label = null, message = null;
+
+    // Parse query
+    String query = uri.getQuery();
+    if(query != null) {
+      try {
+        for (String param : query.split("&")) {
+          String pair[] = param.split("=");
+          String key;
+          key = URLDecoder.decode(pair[0], "UTF-8");
+          String value = null;
+          if (pair.length > 1) {
+            value = URLDecoder.decode(pair[1], "UTF-8");
+          }
+
+          if("amount".equals(key)) {
+            amount = value;
+          } else if("label".equals(key)) {
+            label = value;
+          } else if("message".equals(key)) {
+            message = value;
+          }
+        }
+      } catch (UnsupportedEncodingException e) {
+        // Will never happen
+        throw new RuntimeException(e);
+      }
+    }
+
+    if(address == null) {
+
+      Log.e("Coinbase", "bitcoin: URI had no address (" + uri + ")");
+      return;
+    }
+    
+    mAmount = amount;
+    mNotes = message;
+    mRecipient = address;
+    mTransferType = 0;
+
+    if(mTransferTypeView != null) {
+      mTransferTypeView.setSelection(0); // SEND
+      mAmountView.setText(amount);
+      mNotesView.setText(message);
+      mRecipientView.setText(address);
+    }
   }
 }

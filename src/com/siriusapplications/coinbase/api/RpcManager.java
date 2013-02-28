fc330f5227971b17b20a20a4331085f02ed6df9a
Isaac Waller <isaac@siriusapplications.com>
2013-02-27 17:38:26 -0800

    Added PUT and DELETE methods to RpcManager.

--- src/com/siriusapplications/coinbase/api/RpcManager.java
+++ src/com/siriusapplications/coinbase/api/RpcManager.java
@@ -7,8 +7,11 @@ import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -19,9 +22,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import android.util.Log;
 import android.content.Context;
-import com.siriusapplications.coinbase.api.LoginManager;
+import android.util.Log;
 
 
 public class RpcManager {
@@ -36,6 +38,13 @@ public class RpcManager {
 
     return INSTANCE;
   }
+  
+  private static enum RequestVerb {
+    GET,
+    POST,
+    PUT,
+    DELETE;
+  }
 
   private RpcManager() {
 
@@ -45,20 +54,30 @@ public class RpcManager {
 
   public JSONObject callGet(Context context, String method) throws IOException, JSONException {
 
-    return call(context, method, false, null, true);
+    return call(context, method, RequestVerb.GET, null, true);
   }
 
   public JSONObject callGet(Context context, String method, Collection<BasicNameValuePair> params) throws IOException, JSONException {
 
-    return call(context, method, false, params, true);
+    return call(context, method, RequestVerb.GET, params, true);
   }
   
   public JSONObject callPost(Context context, String method, Collection<BasicNameValuePair> params) throws IOException, JSONException {
     
-    return call(context, method, true, params, true);
+    return call(context, method, RequestVerb.POST, params, true);
+  }
+  
+  public JSONObject callPut(Context context, String method, Collection<BasicNameValuePair> params) throws IOException, JSONException {
+    
+    return call(context, method, RequestVerb.PUT, params, true);
+  }
+  
+  public JSONObject callDelete(Context context, String method, Collection<BasicNameValuePair> params) throws IOException, JSONException {
+    
+    return call(context, method, RequestVerb.DELETE, params, true);
   }
 
-  private JSONObject call(Context context, String method, boolean isPost, Collection<BasicNameValuePair> params, boolean retry) throws IOException, JSONException {
+  private JSONObject call(Context context, String method, RequestVerb verb, Collection<BasicNameValuePair> params, boolean retry) throws IOException, JSONException {
 
     DefaultHttpClient client = new DefaultHttpClient();
 
@@ -66,17 +85,33 @@ public class RpcManager {
 
     HttpUriRequest request = null;
 
-    if(isPost) {
-      request = new HttpPost(url);
+    if(verb == RequestVerb.POST || verb == RequestVerb.PUT || verb == RequestVerb.DELETE) {
+      
+      // Post body is used.
+      switch(verb) {
+      case POST:
+        request = new HttpPost(url);
+        break;
+      case PUT:
+        request = new HttpPut(url);
+        break;
+      case DELETE:
+        request = new HttpDelete(url);
+        break;
+      default:
+        throw new RuntimeException("RequestVerb not implemented: " + verb);
+      }
+      
       List<BasicNameValuePair> parametersBody = new ArrayList<BasicNameValuePair>();
       
       if(params != null) {
         parametersBody.addAll(params);
       }
       
-      ((HttpPost) request).setEntity(new UrlEncodedFormEntity(parametersBody, HTTP.UTF_8));
+      ((HttpEntityEnclosingRequestBase) request).setEntity(new UrlEncodedFormEntity(parametersBody, HTTP.UTF_8));
     } else {
       
+      // URL parameters are used.
       if(params != null) {
         List<BasicNameValuePair> paramsList = (params instanceof List<?>) ? (List<BasicNameValuePair>)params : new ArrayList<BasicNameValuePair>(params);
         url = url + "?" + URLEncodedUtils.format(paramsList, "UTF-8");
@@ -97,7 +132,7 @@ public class RpcManager {
       // This may be caused by an outdated access token - attempt to fetch a new one
       // before failing.
       LoginManager.getInstance().refreshAccessToken(context);
-      return call(context, method, isPost, params, false);
+      return call(context, method, verb, params, false);
     } else if(code != 200) {
 
       throw new IOException("HTTP response " + code + " to request " + method);

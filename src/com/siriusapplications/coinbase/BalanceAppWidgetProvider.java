7475d98bcfb4b06e5f64012c6657ced49218a49d
Isaac Waller <isaac@siriusapplications.com>
2013-02-28 11:56:28 -0800

    Balance on widgets now works

--- src/com/siriusapplications/coinbase/BalanceAppWidgetProvider.java
+++ src/com/siriusapplications/coinbase/BalanceAppWidgetProvider.java
@@ -5,39 +5,59 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.RemoteViews;
 
+import com.siriusapplications.coinbase.UpdateWidgetBalanceService.WidgetUpdater;
+
 public class BalanceAppWidgetProvider extends AppWidgetProvider {
 
-  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+  public static class BalanceWidgetUpdater implements WidgetUpdater {
 
-    for (int i = 0; i < appWidgetIds.length; i++) {
-      int appWidgetId = appWidgetIds[i];
+    @Override
+    public void updateWidget(Context context, AppWidgetManager manager,
+        int appWidgetId, String balance) {
 
       RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_balance);
 
       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-        setKeyguardPadding(context, appWidgetManager, appWidgetId, views);
+        setKeyguardPadding(context, manager, appWidgetId, views);
       }
-      
-      
 
-      appWidgetManager.updateAppWidget(appWidgetId, views);
+      views.setTextViewText(R.id.widget_balance, balance);
+
+      manager.updateAppWidget(appWidgetId, views);
     }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setKeyguardPadding(Context context, AppWidgetManager appWidgetManager, int appWidgetId, RemoteViews views) {
+
+      Bundle myOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
+      int category = myOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
+      boolean isKeyguard = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;
+
+      if(isKeyguard) {
+        int padding = (int) (8 * context.getResources().getDisplayMetrics().density);
+        views.setViewPadding(R.id.widget_outer, padding, padding, padding, padding);
+      }
+    }
+
   }
 
-  @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-  private void setKeyguardPadding(Context context, AppWidgetManager appWidgetManager, int appWidgetId, RemoteViews views) {
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-    Bundle myOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
-    int category = myOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY, -1);
-    boolean isKeyguard = category == AppWidgetProviderInfo.WIDGET_CATEGORY_KEYGUARD;
+    for (int i = 0; i < appWidgetIds.length; i++) {
 
-    if(isKeyguard) {
-      int padding = (int) (8 * context.getResources().getDisplayMetrics().density);
-      views.setViewPadding(R.id.widget_outer, padding, padding, padding, padding);
+      // First, update the widget immediately without balance
+      new BalanceWidgetUpdater().updateWidget(context, appWidgetManager, appWidgetIds[i], null);
+      
+      // Then, start the service to update the widget with balance
+      Intent service = new Intent(context, UpdateWidgetBalanceService.class);
+      service.putExtra(UpdateWidgetBalanceService.EXTRA_UPDATER_CLASS, BalanceWidgetUpdater.class);
+      service.putExtra(UpdateWidgetBalanceService.EXTRA_WIDGET_ID, appWidgetIds[i]);
+      context.startService(service);
     }
   }
 }
--- src/com/siriusapplications/coinbase/TransactionsAppWidgetProvider.java
+++ src/com/siriusapplications/coinbase/TransactionsAppWidgetProvider.java
@@ -10,29 +10,48 @@ import android.net.Uri;
 import android.os.Build;
 import android.widget.RemoteViews;
 
+import com.siriusapplications.coinbase.UpdateWidgetBalanceService.WidgetUpdater;
+
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class TransactionsAppWidgetProvider extends AppWidgetProvider {
+  
+  public static class TransactionsWidgetUpdater implements WidgetUpdater {
+
+    @Override
+    public void updateWidget(Context context, AppWidgetManager manager, int appWidgetId, String balance) {
+      
+      Intent intent = new Intent(context, TransactionsRemoteViewsService.class);
+      intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+      intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+      
+      RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_transactions);
+      rv.setRemoteAdapter(appWidgetId, R.id.widget_list, intent);
+      rv.setEmptyView(R.id.widget_list, R.id.widget_empty);
+      
+      Intent intentTemplate = new Intent(context, TransactionDetailsActivity.class);
+      PendingIntent pendingIntentTemplate = PendingIntent.getActivity(context, 0, intentTemplate, 0);
+      rv.setPendingIntentTemplate(R.id.widget_list, pendingIntentTemplate);
+      
+      rv.setTextViewText(R.id.widget_balance, balance);
+
+      manager.updateAppWidget(appWidgetId, rv); 
+    }
+    
+  }
 
   public void onUpdate(Context context, AppWidgetManager appWidgetManager,
       int[] appWidgetIds) {
     
     for (int i = 0; i < appWidgetIds.length; ++i) {
 
-      Intent intent = new Intent(context, TransactionsRemoteViewsService.class);
-      intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-      intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+      // First, update the widget immediately without balance
+      new TransactionsWidgetUpdater().updateWidget(context, appWidgetManager, appWidgetIds[i], null);
       
-      RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_transactions);
-      rv.setRemoteAdapter(appWidgetIds[i], R.id.widget_list, intent);
-      rv.setEmptyView(R.id.widget_list, R.id.widget_empty);
-      
-      Intent intentTemplate = new Intent(context, TransactionDetailsActivity.class);
-      PendingIntent pendingIntentTemplate = PendingIntent.getActivity(context, 0, intentTemplate, 0);
-      rv.setPendingIntentTemplate(R.id.widget_list, pendingIntentTemplate);
-
-      appWidgetManager.updateAppWidget(appWidgetIds[i], rv);   
+      // Then, start the service to update the widget with balance
+      Intent service = new Intent(context, UpdateWidgetBalanceService.class);
+      service.putExtra(UpdateWidgetBalanceService.EXTRA_UPDATER_CLASS, TransactionsWidgetUpdater.class);
+      service.putExtra(UpdateWidgetBalanceService.EXTRA_WIDGET_ID, appWidgetIds[i]);
+      context.startService(service);
     }
-    
-    super.onUpdate(context, appWidgetManager, appWidgetIds);
   }
 }

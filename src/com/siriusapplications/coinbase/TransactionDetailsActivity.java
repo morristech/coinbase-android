e3f394bb907952d5f63d901ba3b7eb2eb88ea414
Isaac Waller <isaac@siriusapplications.com>
2013-03-02 11:31:36 -0800

    Register for coinbase trasaction links from emails and open them in-app

--- src/com/siriusapplications/coinbase/TransactionDetailsActivity.java
+++ src/com/siriusapplications/coinbase/TransactionDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.siriusapplications.coinbase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
@@ -17,7 +18,16 @@ public class TransactionDetailsActivity extends SherlockFragmentActivity {
     if(savedInstanceState == null) {
 
       Fragment f = new TransactionDetailsFragment();
-      f.setArguments(getIntent().getExtras());
+      
+      Bundle args = null;
+      if(getIntent().getData() == null) {
+        args = getIntent().getExtras();
+      } else {
+        args = new Bundle();
+        args.putParcelable("data", getIntent().getData());
+      }
+      
+      f.setArguments(args);
 
       getSupportFragmentManager().beginTransaction().add(android.R.id.content, f).commit();
     }
@@ -30,6 +40,9 @@ public class TransactionDetailsActivity extends SherlockFragmentActivity {
     
     if(item.getItemId() == android.R.id.home) {
       // Action bar up button
+      Intent intent = new Intent(this, MainActivity.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      startActivity(intent);
       finish();
     }
     
--- src/com/siriusapplications/coinbase/TransactionDetailsFragment.java
+++ src/com/siriusapplications/coinbase/TransactionDetailsFragment.java
@@ -1,11 +1,9 @@
 package com.siriusapplications.coinbase;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -16,6 +14,7 @@ import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -115,149 +114,223 @@ public class TransactionDetailsFragment extends Fragment {
 
   }
 
-  @SuppressLint("SimpleDateFormat")
+  private class LoadTransactionFromInternetTask extends AsyncTask<String, Void, JSONObject> {
+
+    String mId;
+
+    @Override
+    protected JSONObject doInBackground(String... params) {
+
+      mId = params[0];
+
+      try {
+        return RpcManager.getInstance().callGet(getActivity(), "transactions/" + mId).getJSONObject("transaction");
+      } catch (JSONException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject result) {
+
+      if(mView != null && getActivity() != null) {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        int activeAccount = prefs.getInt(Constants.KEY_ACTIVE_ACCOUNT, -1);
+        String currentUserId = prefs.getString(String.format(Constants.KEY_ACCOUNT_ID, activeAccount), null);
+
+        try {
+          if(result != null) {
+            mView.setVisibility(View.VISIBLE);
+            fillViewsForJson(mView, result, currentUserId, mId);
+          }
+          return;
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+
+        // Error
+        Toast.makeText(getActivity(), R.string.transactiondetails_error, Toast.LENGTH_SHORT).show();
+        getActivity().finish();
+      }
+    }
+
+  }
+
+  ViewGroup mView;
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    mView = null;
+  }
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
 
     // Inflate base layout
     ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_transactiondetails, container, false);
+    mView = view;
 
     // Get user ID
     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
     int activeAccount = prefs.getInt(Constants.KEY_ACTIVE_ACCOUNT, -1);
     String currentUserId = prefs.getString(String.format(Constants.KEY_ACCOUNT_ID, activeAccount), null);
 
-    // Fetch transaction JSON from database
-    final String transactionId = getArguments().getString(EXTRA_ID);
-    TransactionsDatabase dbHelper = new TransactionsDatabase(getActivity());
-    SQLiteDatabase db = dbHelper.getReadableDatabase();
-    Cursor c = db.query(TransactionEntry.TABLE_NAME, new String[] { TransactionEntry.COLUMN_NAME_JSON },
-        TransactionEntry._ID + " = ?", new String[] { transactionId }, null, null, null);
-    c.moveToFirst();
+    // Get arguments
+    Bundle args = getArguments();
+    if(args.containsKey("data")) {
+      // From browser link
+      // Fetch transaction information from internet.
+      Uri uri = args.getParcelable("data");
+      String transactionId = uri.getPath().substring("/transactions/".length());
+      new LoadTransactionFromInternetTask().execute(transactionId);
+      mView.setVisibility(View.GONE);
+    } else {
 
-    JSONObject data;
-    try {
-      data = new JSONObject(new JSONTokener(c.getString(c.getColumnIndex(TransactionEntry.COLUMN_NAME_JSON))));
+      // Fetch transaction JSON from database
+      final String transactionId = getArguments().getString(EXTRA_ID);
+      TransactionsDatabase dbHelper = new TransactionsDatabase(getActivity());
+      SQLiteDatabase db = dbHelper.getReadableDatabase();
+      Cursor c = db.query(TransactionEntry.TABLE_NAME, new String[] { TransactionEntry.COLUMN_NAME_JSON },
+          TransactionEntry._ID + " = ?", new String[] { transactionId }, null, null, null);
+      c.moveToFirst();
 
-      c.close();
-
-      // Fill views
-      TextView amount = (TextView) view.findViewById(R.id.transactiondetails_amount),
-          amountLabel = (TextView) view.findViewById(R.id.transactiondetails_label_amount),
-          from = (TextView) view.findViewById(R.id.transactiondetails_from),
-          to = (TextView) view.findViewById(R.id.transactiondetails_to),
-          date = (TextView) view.findViewById(R.id.transactiondetails_date),
-          status = (TextView) view.findViewById(R.id.transactiondetails_status),
-          notes = (TextView) view.findViewById(R.id.transactiondetails_notes);
-      Button resend = (Button) view.findViewById(R.id.transactiondetails_request_resend),
-          cancel = (Button) view.findViewById(R.id.transactiondetails_request_cancel),
-          send = (Button) view.findViewById(R.id.transactiondetails_request_send),
-          decline = (Button) view.findViewById(R.id.transactiondetails_request_decline);
-
-      boolean sentToMe = data.optJSONObject("sender") == null || !currentUserId.equals(data.getJSONObject("sender").optString("id"));
-
-      // Amount
-      String amountText = Utils.formatCurrencyAmount(data.getJSONObject("amount").getString("amount"), true) + " " +
-          data.getJSONObject("amount").getString("currency");
-      amount.setText(amountText);
-      amountLabel.setText(sentToMe ? R.string.transactiondetails_amountreceived : R.string.transactiondetails_amountsent);
-
-      // To / From
-      String recipient = getName(data.optJSONObject("recipient"), data.optString("recipient_address"), currentUserId);
-      from.setText(getName(data.optJSONObject("sender"), null, currentUserId));
-      to.setText(recipient);
-
-      // Date
-      SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy, 'at' hh:mma zzz");
+      JSONObject data;
       try {
-        date.setText(dateFormat.format(ISO8601.toCalendar(data.optString("created_at")).getTime()));
-      } catch (ParseException e) {
-        date.setText(null);
+        data = new JSONObject(new JSONTokener(c.getString(c.getColumnIndex(TransactionEntry.COLUMN_NAME_JSON))));
+
+        c.close();
+
+        fillViewsForJson(view, data, currentUserId, transactionId);
+
+      } catch (JSONException e) {
+        Toast.makeText(getActivity(), R.string.transactiondetails_error, Toast.LENGTH_LONG).show();
+        e.printStackTrace();
+        getActivity().finish();
+      } finally {
+
+        c.close();
+        db.close();
       }
-
-      // Status
-      String transactionStatus = data.optString("status", getString(R.string.transaction_status_error));
-      String readable = transactionStatus;
-      int background = R.drawable.transaction_unknown;
-      if("complete".equals(transactionStatus)) {
-        readable = getString(R.string.transaction_status_complete);
-        background = R.drawable.transaction_complete;
-      } else if("pending".equals(transactionStatus)) {
-        readable = getString(R.string.transaction_status_pending);
-        background = R.drawable.transaction_pending;
-      }
-      status.setText(readable);
-      status.setBackgroundResource(background);
-
-      // Notes
-      String notesText = data.optString("notes");
-      notes.setText("null".equals(notesText) ? null : notesText);
-
-      // Buttons
-      boolean isBuy = data.optJSONObject("sender") != null && "transfers@coinbase.com".equals(data.optJSONObject("sender").optString("email"));
-      boolean isSell = data.optJSONObject("recipient") != null && "transfers@coinbase.com".equals(data.optJSONObject("recipient").optString("email"));
-      boolean senderOrRecipientIsExternal = data.optJSONObject("sender") == null || data.optJSONObject("recipient") == null;
-      if(isBuy || isSell || senderOrRecipientIsExternal || !"pending".equals(transactionStatus)) {
-        cancel.setVisibility(View.GONE);
-        resend.setVisibility(View.GONE);
-        send.setVisibility(View.GONE);
-        decline.setVisibility(View.GONE);
-      } else if(sentToMe) {
-
-        cancel.setVisibility(View.VISIBLE);
-        resend.setVisibility(View.VISIBLE);
-        send.setVisibility(View.GONE);
-        decline.setVisibility(View.GONE);
-      } else {
-
-        cancel.setVisibility(View.GONE);
-        resend.setVisibility(View.GONE);
-        send.setVisibility(View.VISIBLE);
-        decline.setVisibility(View.VISIBLE);
-
-        send.setText(String.format(getString(R.string.transactiondetails_request_send), amountText, recipient));
-      }
-
-      cancel.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          new TakeActionTask().execute(ActionType.CANCEL, transactionId);
-        }
-      });
-
-      resend.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          new TakeActionTask().execute(ActionType.RESEND, transactionId);
-        }
-      });
-
-      send.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          new TakeActionTask().execute(ActionType.COMPLETE, transactionId);
-        }
-      });
-
-      decline.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          new TakeActionTask().execute(ActionType.CANCEL, transactionId);
-        }
-      });
-
-    } catch (JSONException e) {
-      Toast.makeText(getActivity(), R.string.transactiondetails_error, Toast.LENGTH_LONG).show();
-      e.printStackTrace();
-      getActivity().finish();
-    } finally {
-
-      c.close();
-      db.close();
     }
 
     return view;
+  }
+
+  @SuppressLint("SimpleDateFormat")
+  private void fillViewsForJson(ViewGroup view, JSONObject data, String currentUserId,
+      final String transactionId) throws JSONException {
+
+    // Fill views
+    TextView amount = (TextView) view.findViewById(R.id.transactiondetails_amount),
+        amountLabel = (TextView) view.findViewById(R.id.transactiondetails_label_amount),
+        from = (TextView) view.findViewById(R.id.transactiondetails_from),
+        to = (TextView) view.findViewById(R.id.transactiondetails_to),
+        date = (TextView) view.findViewById(R.id.transactiondetails_date),
+        status = (TextView) view.findViewById(R.id.transactiondetails_status),
+        notes = (TextView) view.findViewById(R.id.transactiondetails_notes);
+    Button resend = (Button) view.findViewById(R.id.transactiondetails_request_resend),
+        cancel = (Button) view.findViewById(R.id.transactiondetails_request_cancel),
+        send = (Button) view.findViewById(R.id.transactiondetails_request_send),
+        decline = (Button) view.findViewById(R.id.transactiondetails_request_decline);
+
+    boolean sentToMe = data.optJSONObject("sender") == null || !currentUserId.equals(data.getJSONObject("sender").optString("id"));
+
+    // Amount
+    String amountText = Utils.formatCurrencyAmount(data.getJSONObject("amount").getString("amount"), true) + " " +
+        data.getJSONObject("amount").getString("currency");
+    amount.setText(amountText);
+    amountLabel.setText(sentToMe ? R.string.transactiondetails_amountreceived : R.string.transactiondetails_amountsent);
+
+    // To / From
+    String recipient = getName(data.optJSONObject("recipient"), data.optString("recipient_address"), currentUserId);
+    from.setText(getName(data.optJSONObject("sender"), null, currentUserId));
+    to.setText(recipient);
+
+    // Date
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy, 'at' hh:mma zzz");
+    try {
+      date.setText(dateFormat.format(ISO8601.toCalendar(data.optString("created_at")).getTime()));
+    } catch (ParseException e) {
+      date.setText(null);
+    }
+
+    // Status
+    String transactionStatus = data.optString("status", getString(R.string.transaction_status_error));
+    String readable = transactionStatus;
+    int background = R.drawable.transaction_unknown;
+    if("complete".equals(transactionStatus)) {
+      readable = getString(R.string.transaction_status_complete);
+      background = R.drawable.transaction_complete;
+    } else if("pending".equals(transactionStatus)) {
+      readable = getString(R.string.transaction_status_pending);
+      background = R.drawable.transaction_pending;
+    }
+    status.setText(readable);
+    status.setBackgroundResource(background);
+
+    // Notes
+    String notesText = data.optString("notes");
+    notes.setText("null".equals(notesText) ? null : notesText);
+
+    // Buttons
+    boolean isBuy = data.optJSONObject("sender") != null && "transfers@coinbase.com".equals(data.optJSONObject("sender").optString("email"));
+    boolean isSell = data.optJSONObject("recipient") != null && "transfers@coinbase.com".equals(data.optJSONObject("recipient").optString("email"));
+    boolean senderOrRecipientIsExternal = data.optJSONObject("sender") == null || data.optJSONObject("recipient") == null;
+    if(isBuy || isSell || senderOrRecipientIsExternal || !"pending".equals(transactionStatus)) {
+      cancel.setVisibility(View.GONE);
+      resend.setVisibility(View.GONE);
+      send.setVisibility(View.GONE);
+      decline.setVisibility(View.GONE);
+    } else if(sentToMe) {
+
+      cancel.setVisibility(View.VISIBLE);
+      resend.setVisibility(View.VISIBLE);
+      send.setVisibility(View.GONE);
+      decline.setVisibility(View.GONE);
+    } else {
+
+      cancel.setVisibility(View.GONE);
+      resend.setVisibility(View.GONE);
+      send.setVisibility(View.VISIBLE);
+      decline.setVisibility(View.VISIBLE);
+
+      send.setText(String.format(getString(R.string.transactiondetails_request_send), amountText, recipient));
+    }
+
+    cancel.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new TakeActionTask().execute(ActionType.CANCEL, transactionId);
+      }
+    });
+
+    resend.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new TakeActionTask().execute(ActionType.RESEND, transactionId);
+      }
+    });
+
+    send.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new TakeActionTask().execute(ActionType.COMPLETE, transactionId);
+      }
+    });
+
+    decline.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new TakeActionTask().execute(ActionType.CANCEL, transactionId);
+      }
+    });
   }
 
   private String getName(JSONObject person, String address, String currentUserId) {
